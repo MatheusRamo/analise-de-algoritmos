fad23e55b2c1bb964ce2842804e41c508cbaff69
matheus <geof.matheus@gmail.com>
2021-06-07 12:14:12 -0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# analise-de-algoritmos
+# Análise de Algoritmos
+
+# Busca Linear
+
+Dado um vetor A[1...n] contendo n números reais e um número k qualquer, descobrir se k está armazenado em A.
+
+Percorra o vetor, comparando cada posiçaõ com o valor k.
+Se o elemento A[i] é igual a k, devolva i. Se nenhum elemento for igual a k, devolva -1.
+
+```
+BuscaLiner(A, n, k)
+    i = 1
+    enquanto i < n faça
+        se A[i] == k então
+            devolve i
+        i = i + 1
+    devolve -1
+
+```
+
+## Implementação em C
+
+```
+int buscaLinear(int arr[], int n, int k)
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == k) return i;
+    }
+
+    return 0;
+}
+```
---
+++ buscalinear.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int buscaLinear(int arr[], int n, int k)
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == k) return i;
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+    int array[10];
+    int n, k, check;
+    printf("Digite o tamanho do array: ");
+    scanf("%d", &n);
+    printf("\nDigite os elementos do array\n");
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    printf("\nDigite k: ");
+    scanf("%d", &k);
+
+    check = buscaLinear(array, n, k);
+
+    if(check)
+    {
+        printf("\nO valor %d existe no array\n", k);
+    }
+    else
+    {
+        printf("\nO elemento %d não existe no array\n", k);
+    }
+
+
+    return 0;
+}
